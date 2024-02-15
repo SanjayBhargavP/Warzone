d1db54985ae677fa689cdfa6d44e2ff304d46891
Blesslin40266442 <b_augu@live.concordia.ca>
2024-02-15 07:51:19 +0530

    Added MapCheck.java in Tools folder, which if checks mentioned map is present in the folder.

---
+++ Tools/MapCheck.java
@@ -0,0 +1,28 @@
+import java.io.file;
+/**
+ * MapCheck class checks if user mentioned map is present
+ */
+public class MapCheck
+{
+    /**
+     * This method checks if user mentioned map is present.
+     * @param p_enteredFileName - Map Name being entered.
+     * @param p_path - path.
+     */
+    public static boolean checkMap(String p_enteredFileName,String p_path)
+    {
+        p_enteredFileName=p_enteredFileName+".map";
+        File l_directory=new File(p_path);
+        File[] fileList = directory.listFiles();
+        if (fileList != null)
+        {
+            for (File file : fileList)
+            {
+                if (p_enteredFileName.equals(file.getName()))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+}
