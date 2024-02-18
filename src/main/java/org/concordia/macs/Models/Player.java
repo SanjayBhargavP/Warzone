bcea7e4dcff7d2cad3f8495af0c11ffb15ef2d3d
susmitha810 <153788658+susmitha810@users.noreply.github.com>
2024-02-17 20:27:42 -0500

    corrected errors

--- src/main/java/org/concordia/macs/Models/Player.java
+++ src/main/java/org/concordia/macs/Models/Player.java
@@ -19,24 +19,43 @@ public class Player {
     private ArrayList<Order> d_playerOrder = new ArrayList<>();
     private HashMap<String, Continent> d_continent = new HashMap<>();
 
+    /**
+     *  issues the orders given by the player
+     */
     public void issue_order(){
         this.d_playerOrder.add(d_order);
     }
 
+    /**
+     * @return once an order is issued and executed then it gets removed from the list
+     */
     public Order next_order(){
         Order l_order = d_playerOrder.remove(0);
         return l_order;
     }
 
+    /**
+     * gets the name of the player
+     * @return name of the player
+     */
     public String getD_playerName() {
 
         return d_playerName;
     }
 
+    /**
+     * gets the Id of the player
+     * @return Id of the player
+     */
     public int getD_playerId() {
 
         return d_playerId;
     }
+
+    /**
+     * gets the orders given by the player
+     * @return orders given by the player
+     */
     public Order getD_order() {
 
         return d_order;
@@ -47,41 +66,56 @@ public class Player {
         return d_country;
     }
 
+    /**
+     * sets the player's name
+     * @param p_playerName refers to the name of the player
+     */
     public void setD_playerName(String p_playerName) {
 
         this.d_playerName = p_playerName;
     }
 
+    /**
+     * sets the player's Id
+     * @param p_playerId refers to the Id of the player
+     */
     public void setD_playerId(int p_playerId) {
 
         d_playerId = p_playerId;
     }
 
 
-
+    /**
+     * gets the army count
+     * @return the army count
+     */
     public int getD_armyNumber() {
 
         return d_armyNumber;
     }
 
+    /**
+     * sets the army count
+     * @param d_armyNumber refers to the army count
+     */
     public void setD_armyNumber(int d_armyNumber) {
 
         this.d_armyNumber = d_armyNumber;
     }
 
-
-
-
-
+    /**
+     * adds countries to the list of countries owned by the player
+     * @param p_country refers to the list of countries
+     */
     public void addCountry(Country p_country) {
 
         d_country.put(p_country.getD_countryName(), p_country);
     }
 
-
-
-
-
+    /**
+     * sets the orders given by the player
+     * @param p_order refers to the order object
+     */
     public void setD_order(Order p_order) {
         d_order = p_order;
         this.d_armyNumber = this.d_armyNumber - p_order.getD_armyNumber();
@@ -89,19 +123,19 @@ public class Player {
 
     }
 
-
-
     public HashMap<String, Continent> getD_continent() {
 
         return d_continent;
     }
 
+    /**
+     * sets the player continent
+     * @param d_continent refers to the list of continents
+     */
     public void setD_continent(HashMap<String, Continent> d_continent) {
 
         this.d_continent = d_continent;
     }
-
-
 
     /**
      * This is a default constructor
@@ -134,20 +168,16 @@ public class Player {
      * @param p_playerOrder refers to the list of orders of player
      */
 
-
-
-//    public Player(String p_playerName, int p_playerId, HashMap<String, Country> p_playerCountry, ArrayList<Order> p_playerOrder){
-//        super();
-//        this.d_playerName = p_playerName;
-//        this.d_playerId = p_playerId;
-//        this.d_order = p_playerOrder;
-//        this.d_country = p_playerCountry;
-//    }
+    public Player(String p_playerName, int p_playerId, HashMap<String, Country> p_playerCountry, ArrayList<Order> p_playerOrder){
+        super();
+        this.d_playerName = p_playerName;
+        this.d_playerId = p_playerId;
+        this.d_order = p_playerOrder;
+        this.d_country = p_playerCountry;
+    }
 
     /**
-     *
      * Receives the player orders
-     *
      * @return player orders
      */
 
