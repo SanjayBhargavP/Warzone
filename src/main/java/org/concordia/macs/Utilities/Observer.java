8161a5a5a8ddfebb9e07fc347ee36ac2ad9a7172
Blesslin40266442 <b_augu@live.concordia.ca>
2024-03-12 03:33:05 +0530

    Added update method in observer interface

---
+++ src/main/java/org/concordia/macs/Utilities/Observer.java
@@ -0,0 +1,13 @@
+package org.concordia.macs.Utilities;
+
+/**
+ * It's the Observer Interface
+ */
+public interface Observer {
+
+    /**
+     * This function updated the log message
+     * @param p_log - log message to be updated
+     */
+    void update(String p_log);
+}
