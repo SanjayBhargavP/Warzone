e4245438a55d1da4da7c586b706df3b7ff4313fb
Poojitha333 <poojitha.bhupalli333@gmail.com>
2024-03-14 13:01:34 -0400

    create CountryTest.java

---
+++ src/test/java/org/concordia/macs/Models/CountryTest.java
@@ -0,0 +1,83 @@
+package org.concordia.macs.Models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.concordia.macs.Utilities.Connectivity;
+import org.concordia.macs.Utilities.LoadMap;
+
+
+public class CountryTest
+{
+    private Connectivity d_connectivity=new Connectivity();
+
+    /**
+     *
+     * This method is used to loadmap.
+     *
+     */
+
+    @BeforeEach
+    public void addPlayersIfNotThereTest()
+    {
+        LoadMap.loadMap(d_connectivity, "VeryBasic");
+    }
+
+    /**
+     *
+     * Test if getCountryFromID returns correct country for a valid ID
+     *
+     */
+
+    @Test
+    void getCountryFromID_ValidID_ReturnsCorrectCountry()
+    {
+        Country country = Country.getCountryFromID(d_connectivity.getD_countryList(), 5);
+        assertEquals(country.getD_countryName(), "China");
+    }
+
+    /**
+     *
+     * Test if getCountryFromID returns null for an invalid ID
+     *
+     */
+
+
+    @Test
+    void getCountryFromID_InvalidID_ReturnsNull()
+    {
+        Country country = Country.getCountryFromID(d_connectivity.getD_countryList(), 20);
+        assertNull(country);
+    }
+
+    /**
+     *
+     * Test if getCountryFromName returns correct country for a valid name
+     *
+     */
+
+    @Test
+    void getCountryFromName_ValidName_ReturnsCorrectCountry()
+    {
+        Country country = new Country();
+        Country result = country.getCountryFromName(d_connectivity.getD_countryList(), "Germany");
+        assertNotNull(result);
+        assertEquals(result.getD_countryId(), 3);
+    }
+
+    /**
+     *
+     * Test if getCountryFromName returns null for an invalid name
+     *
+     */
+
+    @Test
+    void getCountryFromName_InvalidName_ReturnsNull()
+    {
+        Country country = new Country();
+        Country result = country.getCountryFromName(d_connectivity.getD_countryList(), "NonExistingCountry");
+        assertNull(result);
+    }
+}
