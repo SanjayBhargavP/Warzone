32e4d6031ca02ccb9d3cbaeba24c0bf1f1037b1c
Poojitha333 <poojitha.bhupalli333@gmail.com>
2024-03-14 13:11:34 -0400

    create JUnitTestModels.java

---
+++ src/test/java/org/concordia/macs/Models/JUnitTestModels.java
@@ -0,0 +1,14 @@
+package org.concordia.macs.Models;
+
+import org.junit.platform.suite.api.*;
+@org.junit.platform.suite.api.Suite
+@SelectClasses
+        (
+                {
+                        CountryTest.class
+                }
+        )
+public class JUnitTestModels
+{
+
+}
