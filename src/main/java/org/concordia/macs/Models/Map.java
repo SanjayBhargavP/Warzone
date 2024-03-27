e747c0c95d462edfccf05532d5d8e377ca3ad210
Blesslin40266442 <b_augu@live.concordia.ca>
2024-03-27 09:06:49 +0530

    added check map logic

--- src/main/java/org/concordia/macs/Models/Map.java
+++ src/main/java/org/concordia/macs/Models/Map.java
@@ -1,5 +1,13 @@
 package org.concordia.macs.Models;
 
+import org.concordia.macs.Utilities.Connectivity;
+import org.concordia.macs.Utilities.LoadMap;
+import org.concordia.macs.Utilities.MapCheck;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Class Map defines the map and its properties like mapname and mapexists ,
  * where mapname is the name of the map and
@@ -48,5 +56,40 @@ public class Map {
      */
     public void setD_mapName(String p_mapName) {
         this.d_mapName = p_mapName;
+    }
+
+    public static void checkMap(Connectivity p_connectivity, String p_mapName)
+    {
+        String l_fileName = p_mapName;
+        String l_copyFileName=l_fileName;
+        Map l_map=new Map();
+        File f = new File("");
+        p_connectivity.setD_nameOfMap(p_mapName);
+        String absolute = f.getAbsolutePath();
+        p_connectivity.setD_pathName(absolute+ File.separator+"src/main/resources");
+        l_fileName = absolute + File.separator + "src/main/resources" + File.separator + l_fileName+".map";
+        p_connectivity.setD_FilePathName(l_fileName);
+        l_map.setD_mapName(l_fileName);
+        ArrayList<String> l_fileContent=new ArrayList<String>();
+        if(MapCheck.checkMap(l_copyFileName, p_connectivity.getD_pathName()))
+        {
+            try
+            {
+                Scanner l_input = new Scanner(new File(l_fileName));
+
+                while(l_input.hasNextLine())
+                {
+                    l_fileContent.add(l_input.nextLine());
+                }
+            }catch(Exception e)
+            {
+                System.out.println("File not found exception");
+            }
+        }
+        if(l_fileContent.contains("[Territories]"))
+            ConquestMapLoader.loadMap(p_connectivity, p_mapName);
+        if(l_fileContent.contains("[countries]"))
+            LoadMap.loadMap(p_connectivity, p_mapName);
+
     }
 }
