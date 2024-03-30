1f29fec325a9f1ffc83981f5656fbf1d1813dea2
Mahfuzzur Rahman <104271784+Mahfuzzur-Rahman@users.noreply.github.com>
2024-03-30 17:45:25 -0400

    Final Build

--- src/main/java/org/concordia/macs/State/Reinforcement.java
+++ src/main/java/org/concordia/macs/State/Reinforcement.java
@@ -5,13 +5,20 @@ import org.concordia.macs.Controllers.GameEngine;
 import org.concordia.macs.Utilities.ColorCoding;
 import org.concordia.macs.Utilities.Connectivity;
 import org.concordia.macs.Utilities.PlayersGameplay;
+import org.concordia.macs.Utilities.SaveGame;
 
+import org.concordia.macs.Models.LogEntryBuffer;
 import org.concordia.macs.Models.Order;
+import org.concordia.macs.Models.Player;
+
+import org.concordia.macs.Strategy.HumanPlayerStrategy;
 
 import org.concordia.macs.View.ShowMap;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 
 /**
  *
@@ -19,16 +26,16 @@ import java.util.Scanner;
  * Concrete state representing the game's reinforcement phase.
  * Allows players to deploy armies and proceed to the Attack phase.
  */
-
 public class Reinforcement extends MainPlay 
 {
+	LogEntryBuffer d_logEntryBuffer= new LogEntryBuffer();
 
 	/**
 	 * Constructor for Reinforcement phase.
-	 * 
+	 *
 	 * @param p_ge The associated GameEngine object.
 	 */
-	Reinforcement(GameEngine p_ge) 
+	Reinforcement(GameEngine p_ge)
 	{
 		super(p_ge);
 	}
@@ -36,88 +43,55 @@ public class Reinforcement extends MainPlay
 	/**
 	 * Allows players to deploy armies during the reinforcement phase.
 	 * 
-	 * @param p_connectivity The Connectivity object.
+	 * @param p_connectivity refers to connectivity object
 	 */
 	public void reinforce(Connectivity p_connectivity) 
 	{
 		int l_temp =1;
 		int flag=0;
+
 		ArrayList<String> l_tempName=new ArrayList<>();
+		ArrayList<Player> l_tempPlayerArray = new ArrayList<>();
+
+		for(int i=0; i<l_playersArray.size(); i++)
+		{
+			l_tempPlayerArray.add(l_playersArray.get(i));
+		}
 
 		while(l_temp>0) 
 		{
 			for(int i=0;i<l_playersArray.size();i++)
 			{
 				String l_userOrder="";
-				Order l_order=new Order();
-				boolean l_validUserCommand =false;
+				boolean l_outcomeIssueOrder = false;
 
-				if(l_playersArray.get(i).getD_armyNumber()!=0)
+				if(l_playersArray.get(i).getD_armyCount()!=0 && !(l_tempName.contains(l_playersArray.get(i).getD_playerName())))
 				{
-
+					d_logEntryBuffer.log("Player "+l_playersArray.get(i).getD_playerName()+" deploy your troops:");
 					System.out.println("Player "+l_playersArray.get(i).getD_playerName()+" deploy your troops:");
-					Scanner l_sc = new Scanner(System.in);
+					l_outcomeIssueOrder =l_playersArray.get(i).issue_order();
 
-					if(ge.getCheckIfTest())
+					if(l_outcomeIssueOrder == false)
 					{
-						int l_countryID = l_playersArray.get(i).getD_country().get(0).getD_countryId();
-						int l_armycount = l_playersArray.get(i).getD_armyNumber();
+						System.out.println("Player "+l_playersArray.get(i).getD_playerName()+" has decided not to deploy any troops");
+						l_tempName.add(l_playersArray.get(i).getD_playerName());
+						flag++;
 
-						l_userOrder = "deploy "+l_countryID+" "+l_armycount;
-						System.out.println("For testcase, we have the following command\n"+l_userOrder);
-					}
-
-					else
-						l_userOrder=l_sc.nextLine();
-
-					if(l_userOrder.equalsIgnoreCase("exit"))
-					{
-						System.out.println("Thank you for Playing the Game");
-						System.exit(0);
-					}
-
-					String[] l_tempOrderListArray=l_userOrder.split(" ");
-
-					for(int j=0;j<l_playersArray.get(i).getD_country().size();j++)
-					{
-						if(Integer.parseInt(l_tempOrderListArray[1])==(l_playersArray.get(i).getD_country().get(j).getD_countryId()))
+						for(int k=0; k<l_tempPlayerArray.size(); k++)
 						{
-							l_order.setD_sourceCountry(l_playersArray.get(i).getD_country().get(j));
-							l_validUserCommand= true;
-						}
-
-					}
-
-					if(l_validUserCommand)
-					{
-						if(PlayersGameplay.checkArmyAvailable(Integer.parseInt(l_tempOrderListArray[2]),l_playersArray.get(i)))
-						{
-							l_order.setD_armyCount(Integer.parseInt(l_tempOrderListArray[2]));
-							l_playersArray.get(i).setD_order(l_order);
-							l_playersArray.get(i).issue_order();
-						}
-
-						else
-						{
-							System.out.println(ColorCoding.ANSI_RED+"Error: Please enter valid number of troops"+ColorCoding.ANSI_RESET);
+							if(l_tempPlayerArray.get(k).getD_playerName().equals(l_playersArray.get(i).getD_playerName()))
+								l_tempPlayerArray.remove(k);
 						}
 					}
-
-					else
-					{
-						System.out.println(ColorCoding.ANSI_RED+"INVALID Command as player "
-								+ l_playersArray.get(i).getD_playerName()+" doesn't control country with countryID "
-								+l_tempOrderListArray[1]+ColorCoding.ANSI_RESET);
-					}
+						
 
 					for(int j=0;j<l_playersArray.size();j++)
 					{
 						if(l_tempName.contains(l_playersArray.get(j).getD_playerName()))
 							continue;
-
 						else
 						{
-						if(l_playersArray.get(j).getD_armyNumber()==0)
+						if(l_playersArray.get(j).getD_armyCount()==0)
 							{
 								l_tempName.add(l_playersArray.get(j).getD_playerName());
 								flag++;	
@@ -135,37 +109,40 @@ public class Reinforcement extends MainPlay
 			}
 		}
 
-		PlayersGameplay.assignArmiesToPlayers(l_playersArray);
+		PlayersGameplay.assignArmiesToPlayers(l_tempPlayerArray);
 		l_temp=1;
 		flag=0;
 
-		while(l_temp>0) 
+		if(l_tempPlayerArray.size() != 0)
 		{
-
-			for(int i=0;i<l_playersArray.size();i++)
+			while(l_temp>0) 
 			{
-				if(l_playersArray.get(i).getD_armyNumber()!=0)
+				for(int i=0;i<l_tempPlayerArray.size();i++)
 				{
-					l_playersArray.get(i).getD_order().execute(l_playersArray.get(i), l_playersArray.get(i).next_order(),p_connectivity,0,0);
-					if(l_playersArray.get(i).getD_armyNumber()==0)
+					if(l_tempPlayerArray.get(i).getD_armyCount()!=0)
 					{
-						flag+=1;
-					}
-				}
+						l_tempPlayerArray.get(i).getD_Order().execute(l_tempPlayerArray.get(i), l_tempPlayerArray.get(i).next_order(),p_connectivity,0,0);
 
-				if(flag==l_playersArray.size())
-				{
-					l_temp=0;
-					break;
+						if(l_tempPlayerArray.get(i).getD_armyCount()==0)
+						{
+							flag+=1;
+						}
+					}
+
+					if(flag==l_tempPlayerArray.size())
+					{
+						l_temp=0;
+						break;
+					}
+					
 				}
 				
+				
 			}
-			
-			
+			System.out.println(ColorCoding.ANSI_GREEN+"All Armies have been successfully deployed. Enter command to proceed"+ColorCoding.ANSI_RESET);
 		}
 
-		System.out.println(ColorCoding.ANSI_GREEN+"All Armies have been successfully deployed. Enter command to proceed"+ColorCoding.ANSI_RESET);
-		ShowMap.showMap(p_connectivity.getD_continentsList(), p_connectivity.getD_countriesList(), Play.getL_playersArray());
+		ShowMap.showMap(p_connectivity.getD_continentList(), p_connectivity.getD_countryList(), Play.getL_playersArray());
 		System.out.println("reinforcement done");
 		ge.setPhase(new Attack(ge));
 	}
@@ -173,19 +150,18 @@ public class Reinforcement extends MainPlay
 	/**
 	 * Invalid command in Reinforcement phase.
 	 * Players cannot attack during the Reinforcement phase.
-	 * 
+	 *
 	 * @param p_connectivity The Connectivity object.
 	 */
-
 	public void attack(Connectivity p_connectivity) 
 	{
-		printInvalidCommandMessage();
+		printInvalidCommandMessage(); 
 	}
 
 	/**
 	 * Invalid command in Reinforcement phase.
 	 * Players cannot fortify during the Reinforcement phase.
-	 * 
+	 *
 	 * @param p_connectivity The Connectivity object.
 	 */
 	public void fortify(Connectivity p_connectivity) 
@@ -196,7 +172,7 @@ public class Reinforcement extends MainPlay
 	/**
 	 * Moves to the Attack phase.
 	 */
-	public void next() 
+	public void next(Connectivity p_connectivity) 
 	{
 		ge.setPhase(new Attack(ge));
 	}
@@ -204,7 +180,7 @@ public class Reinforcement extends MainPlay
     /**
      * {@inheritDoc}
      */
-
+	@Override
 	public void loadMap(Connectivity p_connectivity, String[] p_commands) 
 	{
 		printInvalidCommandMessage();
@@ -213,7 +189,7 @@ public class Reinforcement extends MainPlay
     /**
      * {@inheritDoc}
      */
-
+	@Override
 	public void validateMap(Connectivity p_connectivity) 
 	{
 		printInvalidCommandMessage();
@@ -222,16 +198,17 @@ public class Reinforcement extends MainPlay
     /**
      * {@inheritDoc}
      */
-
+	@Override
 	public void editCountry(String[] p_commands, Connectivity p_connectivity) 
 	{
+
 		printInvalidCommandMessage();
 	}
 
     /**
      * {@inheritDoc}
      */
-
+	@Override
 	public void editContinent(String[] p_commands, Connectivity p_connectivity) 
 	{
 		printInvalidCommandMessage();
@@ -240,7 +217,7 @@ public class Reinforcement extends MainPlay
     /**
      * {@inheritDoc}
      */
-
+	@Override
 	public void editNeighbor(String[] p_commands, Connectivity p_connectivity) 
 	{
 		printInvalidCommandMessage();
@@ -249,7 +226,7 @@ public class Reinforcement extends MainPlay
     /**
      * {@inheritDoc}
      */
-
+	@Override
 	public void saveMap(Connectivity p_connectivity, String p_mapName) 
 	{
 		printInvalidCommandMessage();
@@ -258,7 +235,6 @@ public class Reinforcement extends MainPlay
     /**
      * {@inheritDoc}
      */
-
 	public void setPlayers(String[] p_commands) 
 	{
 		printInvalidCommandMessage();
@@ -267,7 +243,7 @@ public class Reinforcement extends MainPlay
     /**
      * {@inheritDoc}
      */
-
+	@Override
 	public boolean assignCountries(Connectivity p_connectivity) 
 	{
 		printInvalidCommandMessage();
@@ -275,14 +251,30 @@ public class Reinforcement extends MainPlay
 		
 	}
 
-    /**
-     * {@inheritDoc}
-     */
 
-	public void endGame() 
+
+	/**
+	 * Sets the player commands
+	 * @param p_commands refers to commands
+	 * @param p_connectivity refers to connectivity object
+	 */
+	@Override
+	public void setPlayers(String[] p_commands, Connectivity p_connectivity) {
+
+		// Implementation not needed for Reinforcement phase
+	}
+
+	@Override
+	public void enableTournament(String mycommand) {
+		// Implementation not needed for Reinforcement phase
+		
+	}
+
+	public void endGame()
 	{
 		System.out.println("Thank you for Playing the Game");
 		System.exit(0);
-		
+
 	}
+
 }
