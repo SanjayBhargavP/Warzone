feec7e59d064e224c7b0c8f245afa2f2d47e5204
Blesslin40266442 <b_augu@live.concordia.ca>
2024-03-30 07:16:44 +0530

    added logic for end game

--- src/main/java/org/concordia/macs/State/End.java
+++ src/main/java/org/concordia/macs/State/End.java
@@ -3,6 +3,10 @@ package org.concordia.macs.State;
 import org.concordia.macs.Controllers.GameEngine;
 import org.concordia.macs.Utilities.Connectivity;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Represents the end phase of the game.Handles all the action that needs to be performed in end state.
  * @author Blesslin Jeba Shiny
@@ -53,8 +57,9 @@ public class End extends Phase {
     /**
      * Prints the invalid message while trying to set the players
      * @param p_commands - array of user commands
+     * @param p_connectivity - connectivity object
      */
-    public void setPlayers(String[] p_commands)
+    public void setPlayers(String[] p_commands,Connectivity p_connectivity)
     {
         printInvalidCommandMessage();
     }
@@ -100,16 +105,43 @@ public class End extends Phase {
     /**
      * This method ends the game by thank you message.
      */
-    public void endGame()
+    public void endGame(Connectivity p_connectivity)
     {
-        System.out.println("Thank You!");
-        System.exit(0);
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Do you want to save the game?");
+        String choice = "";
+        if(ge.getCheckIfTournament() || ge.getCheckIfTest())
+            choice = "no";
+        else
+            choice = sc.nextLine();
+        if(choice.equalsIgnoreCase("yes"))
+        {
+            System.out.println("Enter the command: ");
+            String[] l_command= sc.nextLine().split(" ");
+            String l_filename=l_command[1];
+            SaveGame savegame = new SaveGame();
+            try
+            {
+                savegame.saveGame(this,p_connectivity,l_filename);
+            } catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("Thank you for Playing the Game");
+            System.exit(0);
+        }
+        else if (choice.equalsIgnoreCase("no"))
+        {
+            System.out.println("Thank you for Playing the Game");
+        }
+        return;
     }
 
     /**
      * Prints the invalid message while trying to issue next.
      */
-    public void next()
+    public void next(Connectivity p_connectivity)
     {
         printInvalidCommandMessage();
     }
@@ -180,6 +212,29 @@ public class End extends Phase {
      */
     @Override
     public void editNeighbor(String[] p_commands, Connectivity p_connectivity) {
+
+    }
+
+    /**
+     * This method is not implemented in end phase.
+     * @param p_connectivity - connectivity object
+     * @param p_commands - array of user commands
+     * @param ge - game engine object
+     */
+    @Override
+    public void loadgame(String[] p_commands, Connectivity p_connectivity, GameEngine ge) throws FileNotFoundException {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * This method is not implemented in end phase.
+     * @param mycommand - command provided in tournament
+     */
+    @Override
+    public
+    void enableTournament(String mycommand) {
+        // TODO Auto-generated method stub
 
     }
 
