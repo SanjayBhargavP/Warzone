51c6a6534e6b930f614f7fde49e023b61244f388
susmitha810 <153788658+susmitha810@users.noreply.github.com>
2024-03-30 22:13:50 -0400

    added PlaySetup class

--- src/main/java/org/concordia/macs/State/PlaySetup.java
+++ src/main/java/org/concordia/macs/State/PlaySetup.java
@@ -1,9 +1,12 @@
 package org.concordia.macs.State;
 import org.concordia.macs.Controllers.GameEngine;
+import org.concordia.macs.Models.LogEntryBuffer;
 import org.concordia.macs.Models.Player;
 import org.concordia.macs.Utilities.ColorCoding;
 import org.concordia.macs.Utilities.Connectivity;
 import org.concordia.macs.Utilities.PlayersGameplay;
+
+import java.util.Scanner;
 
 /**
  * @author Susmitha Mamula
@@ -11,6 +14,8 @@ import org.concordia.macs.Utilities.PlayersGameplay;
  * Phase allows players to add, remove players, assign countries and begin the game
  */
 public class PlaySetup extends Play {
+
+    LogEntryBuffer d_logEntryBuffer= new LogEntryBuffer();
 
     /**
      * Constructor for the PlaySetup phase
@@ -21,69 +26,156 @@ public class PlaySetup extends Play {
     }
 
     /**
-     * Sets the players on provided commands basis.
-     * @param p_command Array of user commands for setting up the players.
+     * Sets the players based on the provided commands.
+     *
+     * @param p_commands Array of user commands for setting up players.
      */
-
-    public void setPlayers(String[] p_command) {
-        int i = 1;
-        while (i < p_command.length) {
-            if (p_command[i].equals("-add")) {
+    public void setPlayers(String[] p_commands,Connectivity p_connectivity)
+    {
+        for(int i=1;i<p_commands.length;)
+        {
+            if(ge.getCheckIfTest())
+            {
                 Player l_player = new Player();
-                l_player.setD_playerName(p_command[i + 1]);
-                l_playersArray.add(l_player);
-                System.out.println(ColorCoding.ANSI_GREEN + l_player.getD_playerName() + " added successfully" + ColorCoding.ANSI_RESET);
-                i = i + 2;
-
-            } else if (p_command[i].equals(("-remove"))) {
-                int j = 0;
-                while (j < l_playersArray.size()) {
-                    if (p_command[i + 1].equals(l_playersArray.get(j).getD_playerName())) {
-                        System.out.println(ColorCoding.ANSI_GREEN + l_playersArray.get(j).getD_playerName() + " removed successfully" + ColorCoding.ANSI_RESET);
-                        l_playersArray.remove(j);
-                        i = i + 2;
+                switch(p_commands[i])
+                {
+                    case "Aggressive":
+                        l_player.setStrategy(new AggressivePlayerStrategy(l_player,p_connectivity));
                         break;
+                    case "Human":
+                        l_player.setStrategy(new HumanPlayerStrategy(l_player,p_connectivity));
+                        break;
+                    case "Benevolent":
+                        l_player.setStrategy(new BenevolentPlayerStrategy(l_player,p_connectivity));
+                        break;
+                    case "Random":
+                        l_player.setStrategy(new RandomPlayerStrategy(l_player,p_connectivity));
+                        break;
+                    case "Cheater":
+                        l_player.setStrategy(new CheaterPlayerStrategy(l_player,p_connectivity));
+                        break;
+                    default:
+                        System.out.println(ColorCoding.ANSI_RED+"Invalid strategy name"+ColorCoding.ANSI_RESET);
+                }
+                l_player.setD_playerName(p_commands[i]);
+                l_playersArray.add(l_player);
+                d_logEntryBuffer.log(l_player.getD_playerName() + "added successfully");
+                System.out.println(ColorCoding.ANSI_GREEN+l_player.getD_playerName()+" added successfully"+ColorCoding.ANSI_RESET);
+                i=i+1;
+            }
+            else
+            {
+                if(p_commands[i].equals("-add"))
+                {
+                    Player l_player = new Player();
+                    l_player.setD_playerName(p_commands[i+1]);
+                    int l_flag=0;
+                    Scanner l_sc=new Scanner(System.in);
+                    do {
+                        System.out.println("Enter the behaviour of the player: "+l_player.getD_playerName() +" - Aggressive, Human, Benevolent, Random, Cheater");
+                        String l_strategy = "";
+                        if(ge.getCheckIfTest())
+                            l_strategy = "Aggressive";
+                        else
+                            l_strategy=l_sc.nextLine();
+                        l_flag=0;
+                        switch(l_strategy)
+                        {
+                            case "Aggressive":
+                                l_player.setStrategy(new AggressivePlayerStrategy(l_player,p_connectivity));
+                                l_flag++;
+                                break;
+                            case "Human":
+                                l_player.setStrategy(new HumanPlayerStrategy(l_player,p_connectivity));
+                                l_flag++;
+                                break;
+                            case "Benevolent":
+                                l_player.setStrategy(new BenevolentPlayerStrategy(l_player,p_connectivity));
+                                l_flag++;
+                                break;
+                            case "Random":
+                                l_player.setStrategy(new RandomPlayerStrategy(l_player,p_connectivity));
+                                l_flag++;
+                                break;
+                            case "Cheater":
+                                l_player.setStrategy(new CheaterPlayerStrategy(l_player,p_connectivity));
+                                l_flag++;
+                                break;
+                            default:
+                                System.out.println(ColorCoding.ANSI_RED+"Invalid strategy name"+ColorCoding.ANSI_RESET);
+                                l_flag=0;
+
+                        }
+                    }while(l_flag==0);
+
+
+
+                    l_player.setD_playerName(p_commands[i+1]);
+                    l_playersArray.add(l_player);
+                    d_logEntryBuffer.log(l_player.getD_playerName() + "added successfully");
+                    System.out.println(ColorCoding.ANSI_GREEN+l_player.getD_playerName()+" added successfully"+ColorCoding.ANSI_RESET);
+                    i=i+2;
+
+                }
+                else if(p_commands[i].equals("-remove"))
+                {
+                    for(int j=0;j<l_playersArray.size();j++)
+                    {
+                        if(p_commands[i+1].equals(l_playersArray.get(j).getD_playerName()))
+                        {
+                            d_logEntryBuffer.log(l_playersArray.get(j).getD_playerName()+ "removed successfully");
+                            System.out.println(ColorCoding.ANSI_GREEN+l_playersArray.get(j).getD_playerName()+" removed successfully"+ColorCoding.ANSI_RESET);
+                            l_playersArray.remove(j);
+                            i=i+2;
+                            break;
+                        }
                     }
-                    j++;
                 }
             }
-        }
-        System.out.println("Players have been set");
 
+        }
+        System.out.println("players have been set");
     }
 
     /**
+     * Assigns countries to players.
      *
-     * Allocate countries to players
-     * @param p_connectivity
-     * @return True if the countries assigned are successful, false otherwise.
+     * @param p_connectivity refers to connectivity object
+     * @return True if countries are successfully assigned, false otherwise.
      */
     public boolean assignCountries(Connectivity p_connectivity) {
-        if (l_playersArray.size() > 0) {
-
-            if (PlayersGameplay.assignCountries(l_playersArray, p_connectivity.getD_countriesList(), p_connectivity.getD_continentsList()) == 0) {
-                System.out.println(ColorCoding.ANSI_GREEN + "Countries assigned to players Successfully" + ColorCoding.ANSI_RESET + "\n");
-
-            } else {
+        if(l_playersArray.size()>0)
+        {
+            if(PlayersGameplay.assignCountries(l_playersArray,p_connectivity.getD_countriesList(),p_connectivity.getD_continentsList())==0)
+            {
+                d_logEntryBuffer.log("Countries assigned to players Successfully");
+                System.out.println(ColorCoding.ANSI_GREEN+"Countries assigned to players Successfully"+ColorCoding.ANSI_RESET+"\n");
+            }
+            else
+            {
                 return false;
             }
-        } else {
-            System.out.println(ColorCoding.ANSI_RED + "ERROR: No players to assign Countries" + ColorCoding.ANSI_RESET);
+        }
+        else
+        {
+            d_logEntryBuffer.log("ERROR: No players to assign Countries");
+            System.out.println(ColorCoding.ANSI_RED+"ERROR: No players to assign Countries"+ColorCoding.ANSI_RESET);
             return false;
         }
         System.out.println("countries have been assigned");
-        for (Player p : l_playersArray) {
-            PlayersGameplay.showPlayersCountry(p, 1);
+        for(Player p:l_playersArray)
+        {
+            PlayersGameplay.showPlayersCountry(p,1);
         }
         PlayersGameplay.assignArmiesToPlayers(l_playersArray);
-        System.out.println("Game Begins!!!!!!!!\n");
-        int i = 0;
-        while (i < l_playersArray.size()) {
-            System.out.println("Player" + Integer.sum(i, 1) + "(" + l_playersArray.get(i).getD_playerName() + ") has Army count:"
-                    + l_playersArray.get(i).getD_armyNumber());
-            PlayersGameplay.showPlayersCountry(l_playersArray.get(i), 1);
+        d_logEntryBuffer.log("Game Begins");
+        System.out.println("Game Begins!!!!!!!!!!!\n");
+        for(int i=0;i<l_playersArray.size();i++)
+        {
+            d_logEntryBuffer.log("Player "+ Integer.sum(i,1) +"("+l_playersArray.get(i).getD_playerName()+") has Army Count: "+l_playersArray.get(i).getD_armyNumber());
+            System.out.println("Player "+ Integer.sum(i,1) +"("+l_playersArray.get(i).getD_playerName()+") has Army Count: "+l_playersArray.get(i).getD_armyNumber());
+            PlayersGameplay.showPlayersCountry(l_playersArray.get(i),1);
             System.out.println();
-            i++;
         }
         return true;
     }
@@ -121,11 +213,13 @@ public class PlaySetup extends Play {
         System.exit(0);
     }
 
+
+
     /**
      * Moves to Reinforcement phase
      */
 
-    public void next() {
+    public void next(Connectivity p_connectivity) {
         ge.setPhase(new Reinforcement(ge));
     }
 
@@ -182,6 +276,11 @@ public class PlaySetup extends Play {
          // TODO Auto-generated method stub
          printInvalidCommandMessage();
      }
+    @Override
+    public void enableTournament(String mycommand) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
 
